ee1d327eae9e3176b0b4bdf167333a28ff4ac657
BaguetteManAFK <gouintim000@stu.asd103.org>
2020-03-02 18:14:49 -0800

    Updated code with blinking limelight and correctly named variables.

--- 2020Robot/src/main/java/frc/robot/subsystems/Chassis.java
+++ 2020Robot/src/main/java/frc/robot/subsystems/Chassis.java
@@ -22,10 +22,6 @@ public class Chassis extends SubsystemBase {
   private TalonFX rightmotor2 = new TalonFX(Constants.DRIVE_MOTOR_2_ID);
   private TalonFX leftmotor1 = new TalonFX(Constants.DRIVE_MOTOR_3_ID);
   private TalonFX leftmotor2 = new TalonFX(Constants.DRIVE_MOTOR_4_ID);
-  private CANSparkMax rightmotor1a = new CANSparkMax(1, MotorType.kBrushless);
-  private CANSparkMax rightmotor2a = new CANSparkMax(2, MotorType.kBrushless);
-  private CANSparkMax leftmotor1a = new CANSparkMax(3, MotorType.kBrushless);
-  private CANSparkMax leftmotor2a = new CANSparkMax(4, MotorType.kBrushless);
 
   /**
    * Creates a new Chassis.
@@ -37,21 +33,11 @@ public class Chassis extends SubsystemBase {
     leftmotor2.setInverted(TalonFXInvertType.CounterClockwise);
     rightmotor1.setInverted(TalonFXInvertType.Clockwise);
     rightmotor2.setInverted(TalonFXInvertType.Clockwise);
-    rightmotor2a.follow(rightmotor1a);
-    leftmotor2a.follow(leftmotor1a);
-    rightmotor1a.setOpenLoopRampRate(0.2);
-    rightmotor2a.setOpenLoopRampRate(0.2);
-    leftmotor1a.setOpenLoopRampRate(0.3);
-    leftmotor2a.setOpenLoopRampRate(0.3);
-    rightmotor1a.getInverted();
-    rightmotor2a.getInverted();
   }
 
   public void drive(double leftpower, double rightpower){
 rightmotor1.set(ControlMode.PercentOutput, rightpower);
 leftmotor1.set(ControlMode.PercentOutput, leftpower);
-rightmotor1a.set(rightpower);
-leftmotor1a.set(leftpower);
   }
 
   @Override
--- 2020Robot/src/main/java/frc/robot/subsystems/Shooter.java
+++ 2020Robot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -16,24 +16,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-  private TalonFX Order66 = new TalonFX(Constants.SHOOTER_MOTOR_2);
-  private TalonFX order66 = new TalonFX(Constants.SHOOTER_MOTOR_1);
+  private TalonFX rightshooter = new TalonFX(Constants.SHOOTER_MOTOR_2);
+  private TalonFX leftshooter = new TalonFX(Constants.SHOOTER_MOTOR_1);
   private TalonSRX Feeder = new TalonSRX(Constants.SHOOTER_FEEDER_ID);
   /**
    * Creates a new Shooter.
    */
   public Shooter() {
-    Order66.configOpenloopRamp(0.2);
-    order66.configOpenloopRamp(0.2);
+    rightshooter.configOpenloopRamp(0.2);
+    leftshooter.configOpenloopRamp(0.2);
     Feeder.configOpenloopRamp(0.5);
-    order66.follow(Order66);
-    order66.setInverted(TalonFXInvertType.Clockwise);
-    Order66.setInverted(TalonFXInvertType.CounterClockwise);
+    leftshooter.follow(rightshooter);
+    leftshooter.setInverted(TalonFXInvertType.Clockwise);
+    rightshooter.setInverted(TalonFXInvertType.CounterClockwise);
   }
 
   public void shoot(double Order66Power, double feederPower){
     Feeder.set(ControlMode.PercentOutput, -feederPower);
-    Order66.set(ControlMode.PercentOutput, Order66Power);
+    rightshooter.set(ControlMode.PercentOutput, Order66Power);
     
   }
   
